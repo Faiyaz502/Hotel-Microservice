7c76afa9087bfa39e18d8f19f97291f8a646c98d
Faiyaz502 <www.fahim.faizas@gmail.com>
2026-02-23 17:38:19 +0600

    Kafka Mail.

---
+++ NotifationService/src/main/java/com/example/NotifationService/NotifationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.NotifationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifationServiceApplication.class, args);
+	}
+
+}
---
+++ NotifationService/src/test/java/com/example/NotifationService/NotifationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NotifationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
