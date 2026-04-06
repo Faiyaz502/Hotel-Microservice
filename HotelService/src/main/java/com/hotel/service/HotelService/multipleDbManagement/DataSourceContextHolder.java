f2a85cf71172fdc825d4527704e4e258857a9b9a
Faiyaz502 <www.fahim.faizas@gmail.com>
2026-04-06 14:00:34 +0600

    Elastic Search Implementation

---
+++ HotelService/src/main/java/com/hotel/service/HotelService/multipleDbManagement/DataSourceContextHolder.java
@@ -0,0 +1,9 @@
+package com.hotel.service.HotelService.multipleDbManagement;
+
+public class DataSourceContextHolder {
+    private static final ThreadLocal<String> CONTEXT = new ThreadLocal<>();
+
+    public static void set(String type) { CONTEXT.set(type); }
+    public static String get() { return CONTEXT.get(); }
+    public static void clear() { CONTEXT.remove(); }
+}
