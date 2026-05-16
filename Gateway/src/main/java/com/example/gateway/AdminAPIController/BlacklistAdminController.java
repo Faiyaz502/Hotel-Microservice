b89b180f5395304a1c40f34e17e216644bd5ceae
Faiyaz502 <www.fahim.faizas@gmail.com>
2026-05-16 12:44:06 +0600

    GateWay Security Update

--- Gateway/src/main/java/com/example/gateway/AdminAPIController/BlacklistAdminController.java
+++ Gateway/src/main/java/com/example/gateway/AdminAPIController/BlacklistAdminController.java
@@ -1,9 +1,13 @@
 package com.example.gateway.AdminAPIController;
 
+import com.example.gateway.filter.AuthClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
 import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -16,6 +20,7 @@ public class BlacklistAdminController {
 
     private final ReactiveStringRedisTemplate redisTemplate;
     private static final String BL_PREFIX = "BLACKLIST:";
+    private static final Logger log = LoggerFactory.getLogger(BlacklistAdminController.class);
 
     public BlacklistAdminController(ReactiveStringRedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
@@ -27,6 +32,8 @@ public class BlacklistAdminController {
     @PreAuthorize("hasRole('ADMIN')")
     public Mono<String> banIp(@PathVariable String ip,
                               @RequestParam(defaultValue = "60") int minutes) {
+
+        log.info("Calling the Manual Ban IP Controller ->{}",ip);
         return ReactiveSecurityContextHolder.getContext()
                 .map(sc -> sc.getAuthentication().getName())
                 .flatMap(adminName -> {
@@ -42,6 +49,8 @@ public class BlacklistAdminController {
     @DeleteMapping("/{ip}")
     @PreAuthorize("hasRole('ADMIN')")
     public Mono<String> unbanIp(@PathVariable String ip) {
+        log.info("Calling the Manual UnBan IP Controller ->{}",ip);
+
         return redisTemplate.delete(BL_PREFIX + ip)
                 .flatMap(deletedCount -> {
                     if (deletedCount > 0) {
@@ -55,7 +64,10 @@ public class BlacklistAdminController {
 
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public Flux<String> listBlacklist() {
+    public Flux<String> listBlacklist(UserDetails authenticatedPrincipal) {
+
+        log.info("Calling the  listBlacklist IP Controller by ->{}",authenticatedPrincipal.getUsername());
+
         ScanOptions options = ScanOptions.scanOptions()
                 .match(BL_PREFIX + "*")
                 .count(100)
